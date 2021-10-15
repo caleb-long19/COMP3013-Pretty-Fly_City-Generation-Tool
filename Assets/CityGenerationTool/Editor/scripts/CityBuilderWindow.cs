37c320fa61ea0e24661bfb453fa1acce063a368b
jamount <55700813+jamount@users.noreply.github.com>
2021-10-15 16:16:34 +0100

    feat: districts are now colour coded and dont spawn on top of each other

--- Assets/CityGenerationTool/Editor/scripts/CityBuilderWindow.cs
+++ Assets/CityGenerationTool/Editor/scripts/CityBuilderWindow.cs
@@ -60,6 +60,40 @@ public class CityBuilderWindow : EditorWindow
         newObject.transform.localScale = newObject.transform.localScale * districtSize;
         newObject.GetComponent<Renderer>().material.color = districtColors[gridInt];
 
+        //district spawns at (0,0) so needs to be moved to a space where it is not overlapping with other districts
+        moveDistrict(newObject);
+
         objectBaseName = "";
     }
+
+    //this method will move a district in the x direction until it is no longer overlapping
+    // the plan is to eventually make it move in the direction which requires the least distance to find space 
+    private bool moveDistrict(GameObject newDistrict)
+    {
+        var districts = GameObject.FindGameObjectsWithTag("District");
+        var districtCount = districts.Length;
+
+        if( districtCount > 1)
+        {
+            foreach (var dist in districts)
+            {
+                if (Overlaps(newDistrict.GetComponent<RectTransform>().rect, dist.GetComponent<RectTransform>().rect))
+                {
+                    Vector3 p = newDistrict.transform.position;
+                    p.x = dist.transform.position.x + dist.transform.localScale.x / 2;
+                    newDistrict.transform.position = p;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    // a small method to check if districts overlap
+    bool Overlaps(Rect rA, Rect rB)
+    {
+        return (rA.x < rB.x + rB.width && rA.x + rA.width > rB.x && rA.y < rB.y + rB.height && rA.y + rA.height > rB.y);
+    }
+
+    
 }
