0a9f1f48113acc97bfd2b22c4284b4448e41f2fa
caleb-long19 <caleblong199@gmail.com>
2021-11-16 20:49:44 +0000

    City Generation Tool - Changelog - Random Prefab From Folder
    - Added Random Prefab Method to CityZone Script
    - Added GameObject Arrays - prefabPool & prefabRandom
    - Moved Subfolders in the Resources Folder
    - Renamed Model Folders to match names in the tool menu

--- Assets/CityGenerationTool/Editor/scripts/zoning scripts/CityZone.cs
+++ Assets/CityGenerationTool/Editor/scripts/zoning scripts/CityZone.cs
@@ -13,11 +13,15 @@ public class CityZone : MonoBehaviour
     public float roadWidth = 30f;
 
     private Vector3 planeCorner;
-
     private float defaultSpacing = 0.2f;
 
     GameObject newObj;
 
+    // Used for the random prefab selection
+    public GameObject[] prefabPool;
+    public GameObject[] prefabRandom;
+
+    // Method for randomly generating selected buildings on top of plane
     void Clear(Transform plane)
     {
         int childs = plane.transform.childCount;
@@ -28,20 +32,36 @@ public class CityZone : MonoBehaviour
         }
     }
 
-    //method for randomly generating selected buildings on top of plane
-    // building will be randomly selected
+    // Method used to store 
+    public void RandomPrefab()
+    {
+        //Loads objects from specified folder located in the resource folder
+        prefabPool = Resources.LoadAll<GameObject>("Blender Models/Residential");
+
+        // Stores total number of prefabs in folder.
+        prefabRandom = new GameObject[prefabPool.Length];
+
+        for (int i = 0; i < prefabRandom.Length; i++)
+        {
+            // We will now add three prefabs from prefabPool to prefabRandom array
+            prefabRandom[i] = prefabPool[Random.Range(0, prefabPool.Length)];
+        }
+    }
+
+    // Building will be randomly selected
     public void Generate()
     {
         Transform plane = this.transform.GetChild(0);
         Clear(plane);
         planeCorner = transform.TransformPoint(plane.GetComponent<MeshFilter>().sharedMesh.vertices[10]);
         Vector3 Position = planeCorner;
+        RandomPrefab();
 
-        for (int i = 0; i < buildingList.Count; i++)
+        for (int i = 0; i < prefabRandom.Length; i++)
         {
-            Instantiate(buildingList[i], plane, true);
+            Instantiate(prefabRandom[i], plane, true);
             //setting building properties
-            plane.GetChild(i).name = buildingList[i].name;
+            plane.GetChild(i).name = prefabRandom[i].name;
             plane.GetChild(i).localScale = plane.GetChild(i).localScale / 8;
 
             //positioning of building
