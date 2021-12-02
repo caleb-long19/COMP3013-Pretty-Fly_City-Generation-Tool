8588303bb9978291ec099e0e952ae949ecb8cc7f
caleb-long19 <caleblong199@gmail.com>
2021-12-02 19:45:00 +0000

    City Generation Tool - Changelog - Quadratic and Cubic Linear Interpolation
    - Added Cubic and Quadratic Interpolation
    - The Sprite GameObject now creates a curve between multiple points
    
    - Need to add this system to our road system and generate curves using points

--- Assets/Curve Editor/CurveEditor.cs
+++ Assets/Curve Editor/CurveEditor.cs
@@ -6,14 +6,40 @@ public class CurveEditor : MonoBehaviour
 {
     [SerializeField] private Transform pointA;
     [SerializeField] private Transform pointB;
+    [SerializeField] private Transform pointC;
+    [SerializeField] private Transform pointD;
+
     [SerializeField] private Transform pointAB;
+    [SerializeField] private Transform pointBC;
+    [SerializeField] private Transform pointCD;
+
+    [SerializeField] private Transform pointAB_BC;
+    [SerializeField] private Transform pointBC_CD;
+    [SerializeField] private Transform pointABCD;
     private float interpolateAmount;
 
     private void Update()
     {
-        interpolateAmount = (interpolateAmount + Time.deltaTime) % 1f;
-
         //Linear Interpolation
-        pointAB.position = Vector3.Lerp(pointA.position, pointB.position, interpolateAmount);
+        interpolateAmount = (interpolateAmount + Time.deltaTime) % 1f;
+        pointABCD.position = CubicLerp(pointA.position, pointB.position, pointC.position, pointD.position, interpolateAmount);
+    }
+
+    private Vector3 QuadraticLerp(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        //Quadratic Interpolation
+        Vector3 ab = Vector3.Lerp(a, b, t);
+        Vector3 bc = Vector3.Lerp(b, c, t);
+
+        return Vector3.Lerp(ab, bc, interpolateAmount);
+    }
+
+    private Vector3 CubicLerp(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
+    {
+        //Cubic Interpolation
+        Vector3 ab_bc = QuadraticLerp(a, b, c, t);
+        Vector3 bc_cd = QuadraticLerp(b, c, d, t);
+
+        return Vector3.Lerp(ab_bc, bc_cd, interpolateAmount);
     }
 }
